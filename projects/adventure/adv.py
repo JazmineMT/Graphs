60158b3ba768f3c4f4087c5e76df2630163d0429
JazmineMT <jazmineamcginnis@gmail.com>
2020-10-09 07:38:14 -0700

    Sprint Done

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -3,6 +3,7 @@ from player import Player
 from world import World
 
 import random
+from random import choice
 from ast import literal_eval
 
 # Load world
@@ -29,6 +30,99 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# We'll Instantiate all necessary variables, lists, etc
+rooms_seen = set()
+# rooms_been = list() <-- List for tracing purposes
+player.current_room = world.starting_room
+rooms_seen.add(player.current_room.id)
+last_move = ''
+next_move = ''
+fourway_rooms = dict()
+
+#  We'll set our first move as east.
+next_move = 'n'
+traversal_path.append(next_move)
+
+# Our while loop will continue until all rooms have been visitted
+while len(rooms_seen) < 500:
+    # We move in the chosen dir. and add the room to our visitted set
+    player.travel(next_move)
+    rooms_seen.add(player.current_room.id)
+    # rooms_been.append(player.current_room.id) 
+
+    last_move = next_move
+    # print(player.current_room.get_exits())
+
+    # This automates our traversal of all the rooms
+    if last_move == 'e':
+        if 's' in player.current_room.get_exits():
+            next_move = 's'
+        elif 'e' in player.current_room.get_exits():
+            next_move = 'e'
+        elif 'n' in player.current_room.get_exits():
+            next_move = 'n'
+        else: 
+            next_move = 'w'
+
+    elif last_move == 'n':
+        if 'e' in player.current_room.get_exits():
+            next_move = 'e'
+        elif 'n' in player.current_room.get_exits():
+            next_move = 'n'
+        elif 'w' in player.current_room.get_exits():
+            next_move = 'w'
+        else: 
+            next_move = 's'
+
+    elif last_move == 'w':
+        if 'n' in player.current_room.get_exits():
+            next_move = 'n'
+        elif 'w' in player.current_room.get_exits():
+            next_move = 'w'
+        elif 's' in player.current_room.get_exits():
+            next_move = 's'
+        else: 
+            next_move = 'e'
+
+    elif last_move == 's':
+        if 'w' in player.current_room.get_exits():
+            next_move = 'w'
+        elif 's' in player.current_room.get_exits():
+            next_move = 's'
+        elif 'e' in player.current_room.get_exits():
+            next_move = 'e'
+        else: 
+            next_move = 'n'
+
+    if len(player.current_room.get_exits()) == 4:
+        current = player.current_room.id
+
+        # create a dictionary for the intersection in question
+        if current not in fourway_rooms:
+            fourway_rooms[current] = []
+
+        # add next move to the dictionary if the move is not already present
+        if next_move not in fourway_rooms[current]:
+            fourway_rooms[current].append(next_move)
+
+        # if the next move is present, make a random choice of untraveled directions
+        elif len(fourway_rooms[current]) < 4:
+            next_move = choice([i for i in ['n', 's', 'e', 'w'] if i not in fourway_rooms[current]])
+            fourway_rooms[current].append(next_move)
+
+    # Add next_move to the traversal path
+    traversal_path.append(next_move)
+
+
+
+print('Total Rooms traversed:', len(rooms_seen))
+print('Length of Traversal Path:', len(traversal_path))
+rooms_missed = []
+for i in range(500):
+    if i not in (rooms_seen):
+        rooms_missed.append(i)
+
+# print(rooms_missed) 
 
 
 # TRAVERSAL TEST
@@ -39,6 +133,7 @@ visited_rooms.add(player.current_room)
 for move in traversal_path:
     player.travel(move)
     visited_rooms.add(player.current_room)
+    print(player.current_room.id, player.current_room.get_exits())
 
 if len(visited_rooms) == len(room_graph):
     print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
